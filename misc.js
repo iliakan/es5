1a6e9be66c1d7400fbd04fd89176959e15f79f58
Ilya Kantor <iliakan@gmail.com>
2012-06-14 20:44:19 +0300

    add share js

---
+++ misc.js
@@ -0,0 +1,32 @@
+  window.VK && document.write(VK.Share.button({
+    url: "http://es5.javascript.ru/",
+    title: "EcmaScript 5 на русском",
+    image: "http://learn.javascript.ru/themes/analytic/images/smart.gif",
+    noparse: true}, { type: "round", text: 'Поделиться' }));
+
+
+  !function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id)){
+      js=d.createElement(s);js.id=id;  
+      js.src="//platform.twitter.com/widgets.js";  
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  }(document,"script","twitter-wjs");
+
+  window.___gcfg = {lang: 'ru'};
+
+  (function() {
+    if (document.getElementById('gplus')) return;
+    var po = document.createElement('script'); po.id = 'gplus'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&appId=112959575461438";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
